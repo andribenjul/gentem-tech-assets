08ac9bca823dc0542e46ec517841f557765df637
Andri Benjul <andribenjul@users.noreply.github.com>
2026-07-09 16:17:28 +0700

    fix: Dashborad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Providers>
+          {children}
+          <Toaster richColors position="top-right" />
+        </Providers>
+      </body>
     </html>
   );
 }
